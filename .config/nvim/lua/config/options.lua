1a3cabe1b6f4f69c5003ca8f62f4a20f72740c2d
BEARYDEV <teerapat.tech@gmail.com>
2024-08-25 08:41:40 +0700

    update color logo

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -68,67 +68,67 @@ vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 -- TEST:
 --
 
--- if vim.g.neovide then
---   -- vim.opt.laststatus = 0
---
---   vim.g.neovide_floating_shadow = true
---   vim.g.neovide_floating_z_height = 10
---   vim.g.neovide_light_angle_degrees = 45
---   vim.g.neovide_light_radius = 5
---   vim.g.neovide_show_border = true
---
---   vim.opt.guifont = "ComicShannsMono Nerd Font:h22"
---   vim.g.set_encoding = "utf-8"
---   vim.opt.linespace = 0
---   vim.g.neovide_scale_factor = 1.0
---
---   vim.g.neovide_transparency = 0.85
---   vim.g.transparency = 0.85
---   vim.g.neovide_window_blurred = true
---   vim.g.neovide_floating_blur_amount_x = 10.0
---   vim.g.neovide_floating_blur_amount_y = 10.0
---
---   -- vim.g.neovide_show_border = true
---
---   vim.g.neovide_padding_top = 0
---   vim.g.neovide_padding_bottom = 0
---   vim.g.neovide_padding_right = 0
---   vim.g.neovide_padding_left = 0
---
---   vim.g.neovide_scroll_animation_length = 0.3
---   vim.g.neovide_hide_mouse_when_typing = true
---   vim.g.neovide_underline_automatic_scaling = false
---
---   -- vim.g.neovide_theme = "wombat"
---   vim.g.neovide_theme = "auto"
---
---   vim.g.neovide_refresh_rate = 120
---   vim.g.neovide_refresh_rate_idle = 5
---   vim.g.neovide_no_idle = true
---   -- vim.g.neovide_confirm_quit = true
---   -- vim.g.neovide_fullscreen = true
---   vim.g.neovide_remember_window_size = true
---   -- vim.g.neovide_cursor_animation_length = 0.13
---   vim.g.neovide_cursor_trail_size = 0.7 -- Default is 0.8
---   vim.g.neovide_cursor_antialiasing = true
---   vim.g.neovide_cursor_animate_in_insert_mode = true
---   vim.g.neovide_cursor_animate_command_line = true
---   vim.g.neovide_cursor_unfocused_outline_width = 0.125
---
---   vim.g.neovide_cursor_smooth_blink = true
---
---   -- g.neovide_cursor_vfx_mode = "railgun"
---   -- g.neovide_cursor_vfx_mode = "torpedo"
---   vim.g.neovide_cursor_vfx_mode = "pixiedust"
---   -- g.neovide_cursor_vfx_mode = "sonicboom"
---   -- g.neovide_cursor_vfx_mode = "ripple"
---   -- g.neovide_cursor_vfx_mode = "wireframe"
---   --
---   vim.g.neovide_cursor_vfx_opacity = 200.0
---   vim.g.neovide_cursor_vfx_particle_lifetime = 2.5 -- Default is 1.2
---   vim.g.neovide_cursor_vfx_particle_density = 25.0 -- Default is 7.0
---   vim.g.neovide_cursor_vfx_particle_speed = 10.0 -- Default is 10.0
---
---   -- g.neovide_cursor_vfx_particle_phase = 1.5 -- Only for the railgun vfx mode.
---   -- g.neovide_cursor_vfx_particle_curl = 1.0 -- Only for the railgun vfx mode.
--- end
+if vim.g.neovide then
+  -- vim.opt.laststatus = 0
+
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+  vim.g.neovide_show_border = true
+
+  vim.opt.guifont = "ComicShannsMono Nerd Font:h20"
+  vim.g.set_encoding = "utf-8"
+  vim.opt.linespace = 0
+  vim.g.neovide_scale_factor = 1.0
+
+  vim.g.neovide_transparency = 0.85
+  vim.g.transparency = 0.85
+  vim.g.neovide_window_blurred = true
+  vim.g.neovide_floating_blur_amount_x = 10.0
+  vim.g.neovide_floating_blur_amount_y = 10.0
+
+  -- vim.g.neovide_show_border = true
+
+  vim.g.neovide_padding_top = 0
+  vim.g.neovide_padding_bottom = 0
+  vim.g.neovide_padding_right = 0
+  vim.g.neovide_padding_left = 0
+
+  vim.g.neovide_scroll_animation_length = 0.3
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_underline_automatic_scaling = false
+
+  -- vim.g.neovide_theme = "wombat"
+  vim.g.neovide_theme = "auto"
+
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_refresh_rate_idle = 5
+  vim.g.neovide_no_idle = true
+  -- vim.g.neovide_confirm_quit = true
+  -- vim.g.neovide_fullscreen = true
+  vim.g.neovide_remember_window_size = true
+  -- vim.g.neovide_cursor_animation_length = 0.13
+  vim.g.neovide_cursor_trail_size = 0.7 -- Default is 0.8
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_animate_in_insert_mode = true
+  vim.g.neovide_cursor_animate_command_line = true
+  vim.g.neovide_cursor_unfocused_outline_width = 0.125
+
+  vim.g.neovide_cursor_smooth_blink = true
+
+  -- g.neovide_cursor_vfx_mode = "railgun"
+  -- g.neovide_cursor_vfx_mode = "torpedo"
+  vim.g.neovide_cursor_vfx_mode = "pixiedust"
+  -- g.neovide_cursor_vfx_mode = "sonicboom"
+  -- g.neovide_cursor_vfx_mode = "ripple"
+  -- g.neovide_cursor_vfx_mode = "wireframe"
+  --
+  vim.g.neovide_cursor_vfx_opacity = 200.0
+  vim.g.neovide_cursor_vfx_particle_lifetime = 2.5 -- Default is 1.2
+  vim.g.neovide_cursor_vfx_particle_density = 25.0 -- Default is 7.0
+  vim.g.neovide_cursor_vfx_particle_speed = 10.0 -- Default is 10.0
+
+  -- g.neovide_cursor_vfx_particle_phase = 1.5 -- Only for the railgun vfx mode.
+  -- g.neovide_cursor_vfx_particle_curl = 1.0 -- Only for the railgun vfx mode.
+end
